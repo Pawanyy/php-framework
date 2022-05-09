8769052548e3fd2bfcb81bc6954f19d1342f1f7c
Pawanyyy <Pawanyyy01@gmail.com>
2022-05-10 03:12:22 +0530

    working router

--- core/Router.php
+++ core/Router.php
@@ -3,9 +3,31 @@
 namespace app\core;
 
 class Router{
+
+    public Request $request;
+    protected array $routes = [];
+
     
-    public function __construct()
+    public function __construct(\app\core\Request $request)
     {
-        
+        $this->request = $request;
+    }
+
+    public function get(string $path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+
+        if($callback === false){
+            die("Not Found");
+        }
+
+        call_user_func($callback);
     }
 }
